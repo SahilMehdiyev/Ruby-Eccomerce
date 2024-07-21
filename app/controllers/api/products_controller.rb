e3ac3b812c24c3bf567b5603ab8e3421d993de43
SahilMehdiyev <branchsahil@gmail.com>
2024-07-21 11:28:15 +0400

    crud update islemi

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,12 +1,19 @@
 module Api
 
   class ProductsController < ApplicationController
-    skip_before_action :verify_authenticity_token, only: [:create]
+    skip_before_action :verify_authenticity_token, only: [:create, :update]
 
 
     def create 
       @product = Product.create(product_params)
       @product.save
+      render json: @product
+    end
+
+
+    def update
+      @product = Product.find(params[:id])
+      @product.update(product_params)
       render json: @product
     end
 
