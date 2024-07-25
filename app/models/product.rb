1889562b2dbf23a73f8d388f4b23f4cb134a4a6a
SahilMehdiyev <branchsahil@gmail.com>
2024-07-25 16:17:55 +0400

    custom validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -11,7 +11,18 @@ class Product < ApplicationRecord
   validates :quantity, presence:true, numericality: {grater_than_or_equals: 0, less_than: 100}
   validates :price, presence:true, numericality: {grayder_than: 0}
 
+  #custom validate
+  validate :name_start_with_a
+  validate :custom_description
 
+
+  def name_start_with_a
+    if !self.name.start_with?('a')
+      errors.add(:name, 'a herfi ile baslamalidir!')
+    end
+  end
+
+``
 
   def send_notification
     p 'Yeni bir mehsul elave etdikde istifadecilere notification gonder'
